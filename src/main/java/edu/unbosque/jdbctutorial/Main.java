9f4479afee0971d3d9d7cb20224496f8994b14b1
pipe1990 <afca100@hotmail.com>
2021-10-21 23:32:57 -0500

    hola

---
+++ src/main/java/edu/unbosque/jdbctutorial/Main.java
@@ -0,0 +1,62 @@
+package edu.unbosque.jdbctutorial;
+
+import java.util.*;
+import java.sql.*;
+
+import edu.unbosque.jdbctutorial.dtos.Owner;
+import edu.unbosque.jdbctutorial.dtos.UserApp;
+import edu.unbosque.jdbctutorial.services.OwnersService;
+import edu.unbosque.jdbctutorial.services.PetsService;
+import edu.unbosque.jdbctutorial.services.UsersService;
+
+public class Main {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.postgresql.Driver";
+    static final String DB_URL = "jdbc:postgresql://localhost/fourpaws";
+
+    // Database credentials
+    static final String USER = "postgres";
+    static final String PASS = " ";
+
+    public static void main(String[] args) {
+
+        // Objects for handling connection
+        Connection conn = null;
+
+        try {
+
+            // Registering the JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // Opening database connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            UsersService usersService = new UsersService(conn);
+            usersService.listUsers();
+
+            PetsService petsService = new PetsService(conn);
+            petsService.countBySpecies("Dog");
+
+            OwnersService ownersService = new OwnersService(conn);
+            ownersService.updateOwner(new Owner(6698, null, "Pepito Perez"));
+
+            // Closing database connection
+            conn.close();
+
+        } catch(SQLException se) {
+            se.printStackTrace(); // Handling errors from database
+        } catch(ClassNotFoundException e) {
+            e.printStackTrace(); // Handling errors from JDBC driver
+        } finally {
+            // Cleaning-up environment
+            try {
+                if(conn != null) conn.close();
+            } catch(SQLException se) {
+                se.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/edu/unbosque/jdbctutorial/dtos/Pet.java
@@ -0,0 +1,88 @@
+package edu.unbosque.jdbctutorial.dtos;
+
+public class Pet {
+
+    private String pet_id;
+    private String microchip;
+    private String name;
+    private String species;
+    private String race;
+    private String sex;
+    private String picture;
+    private String owner_id;
+
+    public Pet(String pet_id, String microchip, String name, String species, String race, String sex, String picture, String owner_id) {
+        this.pet_id = pet_id;
+        this.microchip = microchip;
+        this.name = name;
+        this.species = species;
+        this.race = race;
+        this.sex = sex;
+        this.picture = picture;
+        this.owner_id = owner_id;
+    }
+
+    public String getPet_id() {
+        return pet_id;
+    }
+
+    public void setPet_id(String pet_id) {
+        this.pet_id = pet_id;
+    }
+
+    public String getMicrochip() {
+        return microchip;
+    }
+
+    public void setMicrochip(String microchip) {
+        this.microchip = microchip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(String owner_id) {
+        this.owner_id = owner_id;
+    }
+}
